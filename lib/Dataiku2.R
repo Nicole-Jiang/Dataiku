5ba335871a7c2881dad390fc4a2fb0f2a6b1c030
Nicole Jiang <cj2451@columbia.edu>
2017-01-25 17:44:17 -0500

    update

--- lib/Dataiku2.R
+++ lib/Dataiku2.R
@@ -6,62 +6,107 @@ class(train.data[,42])  #Factor
 summary(train.data[,42]) #187140- vs 12382+ # Unbalanced dependent variable!
 names(train.data)[42]= "Saving"
 str(train.data)
-##### Baseline Model: Random Forest. Classification, robust #####
-library(randomForest) 
-## Choose best mtry ##
-sample.index= sample(199522, 1000)
+train.data$label= 0
+train.data$label[train.data$Saving== levels(train.data$Saving)[2]] =1
+train.data$label= as.factor(train.data$label)
+str(train.data)
+sample.index= sample(199522, 20000)
 train.sample= train.data[sample.index,]
 test.sample= train.data[-sample.index,]
-rf= randomForest(Saving~., data= train.sample)
-predict.rf= predict(rf,test.sample)
+
+##### Baseline Model: Random Forest. Classification, robust #####
+library(randomForest) 
+rf.fit= randomForest(train.sample$label~., data= train.sample[,1:41])
+predict.rf= predict(rf.fit,test.sample[,1:41])
 # Check overall accuracy
-sum(predict.rf == test.sample[,42])/length(predict.rf)
+sum(predict.rf == test.sample$label)/length(predict.rf) #0.9405557
 # Check confusion matrix
-confusion.matrix= matrix(ncol=2,nrow=2)
-confusion.matrix[1,1]= sum(predict.rf == levels(predict.rf)[1] & test.sample[,42] == levels(predict.rf)[1])
-confusion.matrix[1,2]= sum(predict.rf == levels(predict.rf)[2] & test.sample[,42] == levels(predict.rf)[1])
-confusion.matrix[2,1]= sum(predict.rf == levels(predict.rf)[1] & test.sample[,42] == levels(predict.rf)[2])
-confusion.matrix[2,2]= sum(predict.rf == levels(predict.rf)[2] & test.sample[,42] == levels(predict.rf)[2])
+a= sum(predict.rf == 0 & test.sample$label== 0)
+b= sum(predict.rf == 1 & test.sample$label== 0)
+c= sum(predict.rf == 0 & test.sample$label== 1)
+d= sum(predict.rf == 1 & test.sample$label== 1)
+confusion.matrix.rf= matrix(c(a,c,b,d),ncol=2,nrow=2)
 #Cohen's Kappa
-p0= (confusion.matrix[1,1]+confusion.matrix[2,2])/sum(confusion.matrix)
-margin1= (confusion.matrix[1,1]+confusion.matrix[1,2])/sum(confusion.matrix)*(confusion.matrix[1,1]+confusion.matrix[2,1])
-margin2= (confusion.matrix[2,1]+confusion.matrix[2,2])/sum(confusion.matrix)*(confusion.matrix[1,2]+confusion.matrix[2,2])
-pe= (margin1 + margin2 )/ sum(confusion.matrix)
-Kappa= (p0-pe)/ (1-pe)
-#Kappa =0.1647331
+p0= (a+d)/(a+b+c+d)
+margin1= (a+b)/(a+b+c+d)*(a+c)
+margin2= (c+d)/(a+b+c+d)*(b+d)
+pe= (margin1 + margin2 )/ (a+b+c+d)
+Kappa.rf= (p0-pe)/ (1-pe)
+#Kappa =0.092
 
 
-
-
-########## boost
+########## boost ########## 
 library(rpart)
 library(ada)
-ada.fit = ada(train.sample$Saving~., data = train.sample, type = 'discrete')
-predict.ada= predict(ada.fit,test.sample)
-sum(predict.ada == test.sample[,42])/length(predict.ada)
-confusion.matrix2= matrix(ncol=2,nrow=2)
-confusion.matrix2[1,1]= sum(predict.ada == levels(predict.ada)[1] & test.sample[,42] == levels(predict.ada)[1])
-confusion.matrix2[1,2]= sum(predict.ada == levels(predict.ada)[2] & test.sample[,42] == levels(predict.ada)[1])
-confusion.matrix2[2,1]= sum(predict.ada == levels(predict.ada)[1] & test.sample[,42] == levels(predict.ada)[2])
-confusion.matrix2[2,2]= sum(predict.ada == levels(predict.ada)[2] & test.sample[,42] == levels(predict.ada)[2])
-p0= (confusion.matrix2[1,1]+confusion.matrix2[2,2])/sum(confusion.matrix2)
-margin1= (confusion.matrix2[1,1]+confusion.matrix2[1,2])/sum(confusion.matrix2)*(confusion.matrix2[1,1]+confusion.matrix2[2,1])
-margin2= (confusion.matrix2[2,1]+confusion.matrix2[2,2])/sum(confusion.matrix2)*(confusion.matrix2[1,2]+confusion.matrix2[2,2])
-pe= (margin1 + margin2 )/ sum(confusion.matrix2)
-Kappa= (p0-pe)/ (1-pe) #0.3455662
-
-### XG boost ###
-
-col= sapply(train.sample,is.numeric)
-a= train.sample[,col]
-train.sample$Saving[train.sample$Saving== levels(predict.ada)[1]]=0
-train.sample$label= 0
-train.sample$label[train.sample$Saving== levels(predict.ada)[2]] =1
-t= factor(as.character(train.sample$Saving),levels= c(0,1))
-library(xgboost)
-dtrain <- xgb.DMatrix(as.matrix(a),label = train.sample$Saving)
-xg.fit= xgboost(dtrain,max.depth = 2,
-                eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
-
-
+ada.fit = ada(train.sample$label~., data = train.sample[,1:41], type = 'discrete')
+predict.ada= predict(ada.fit,test.sample[,1:41])
+sum(predict.ada == test.sample$label)/length(predict.ada) #0.9495362
+a= sum(predict.ada == 0 & test.sample$label== 0)
+b= sum(predict.ada == 1 & test.sample$label== 0)
+c= sum(predict.ada == 0 & test.sample$label== 1)
+d= sum(predict.ada == 1 & test.sample$label== 1)
+confusion.matrix.ada= matrix(c(a,c,b,d),ncol=2,nrow=2)
+#Cohen's Kappa
+p0= (a+d)/(a+b+c+d)
+margin1= (a+b)/(a+b+c+d)*(a+c)
+margin2= (c+d)/(a+b+c+d)*(b+d)
+pe= (margin1 + margin2 )/ (a+b+c+d)
+Kappa.ada= (p0-pe)/ (1-pe) #0.4507
 # To do: Resampling Your Dataset, ensemble
+
+########## resample #############
+index1= which(train.data$label==1)
+sample.index2= sample(c(1:199522,rep(index1,10)),10000)
+train.sample2= train.data[sample.index2,]
+test.sample2= train.data[-sample.index2,]
+# random forest
+rf.fit2= randomForest(train.sample2$label~., data= train.sample2[,1:41])
+predict.rf2= predict(rf.fit2,test.sample2[,1:41])
+# Check overall accuracy
+sum(predict.rf2 == test.sample2$label)/length(predict.rf2) #0.95386
+
+a= sum(predict.rf2 == 0 & test.sample2$label== 0)
+b= sum(predict.rf2 == 1 & test.sample2$label== 0)
+c= sum(predict.rf2 == 0 & test.sample2$label== 1)
+d= sum(predict.rf2 == 1 & test.sample2$label== 1)
+confusion.matrix.rf2= matrix(c(a,c,b,d),ncol=2,nrow=2)
+#Cohen's Kappa
+p0= (a+d)/(a+b+c+d)
+margin1= (a+b)/(a+b+c+d)*(a+c)
+margin2= (c+d)/(a+b+c+d)*(b+d)
+pe= (margin1 + margin2 )/ (a+b+c+d)
+Kappa.rf2= (p0-pe)/ (1-pe) #0.08568694
+
+### ada boost ##
+ada.fit2 = ada(train.sample2$label~., data = train.sample2[,1:41], type = 'discrete')
+predict.ada2= predict(ada.fit2,test.sample2[,1:41])
+sum(predict.ada2 == test.sample2$label)/length(predict.ada2) #0.876914861
+a= sum(predict.ada2 == 0 & test.sample2$label== 0)
+b= sum(predict.ada2 == 1 & test.sample2$label== 0)
+c= sum(predict.ada2 == 0 & test.sample2$label== 1)
+d= sum(predict.ada2 == 1 & test.sample2$label== 1)
+confusion.matrix.ada2= matrix(c(a,c,b,d),ncol=2,nrow=2)
+#Cohen's Kappa
+p0= (a+d)/(a+b+c+d)
+margin1= (a+b)/(a+b+c+d)*(a+c)
+margin2= (c+d)/(a+b+c+d)*(b+d)
+pe= (margin1 + margin2 )/ (a+b+c+d)
+Kappa.ada2= (p0-pe)/ (1-pe) #0.3466
+
+##### final #####
+final.prediction =vector()
+final.prediction[predict.rf == 1 | predict.ada==1]=1
+final.prediction[predict.rf == 0 & predict.ada== 0]= 0
+#final.prediction[predict.ada2 == 1 & predict.rf2== 0]= 0
+sum(final.prediction == test.sample$label)/length(final.prediction) #0.8814861
+a= sum(final.prediction == 0 & test.sample2$label== 0)
+b= sum(final.prediction == 1 & test.sample2$label== 0)
+c= sum(final.prediction == 0 & test.sample2$label== 1)
+d= sum(final.prediction == 1 & test.sample2$label== 1)
+confusion.matrix.f= matrix(c(a,c,b,d),ncol=2,nrow=2)
+#Cohen's Kappa
+p0= (a+d)/(a+b+c+d)
+margin1= (a+b)/(a+b+c+d)*(a+c)
+margin2= (c+d)/(a+b+c+d)*(b+d)
+pe= (margin1 + margin2 )/ (a+b+c+d)
+Kappa.ada2= (p0-pe)/ (1-pe) #0.3466
